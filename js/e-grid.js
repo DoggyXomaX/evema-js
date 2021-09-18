2e7f47063a076ac30f9e279b4478d1a6f0c78382
DoggyXomaX <l_l_l_l-l_l_l_l@mail.ru>
2021-09-18 19:35:47 +0300

    Scripting: Added Rebuild, Redraw, Clear, Draw to grid

--- js/e-grid.js
+++ js/e-grid.js
@@ -1,6 +1,7 @@
 Evema.Grid = {};
 
 Evema.Grid.Instance = null;
+Evema.Grid.Context = null;
 
 Evema.Grid.Options = {
     BackgroundColor: '#FFFFFF',
@@ -22,4 +23,65 @@ Evema.Grid.Init = function() {
     }
 
     Evema.Grid.Instance = instance;
+    Evema.Grid.Context = instance.getContext( '2d' );
+
+    Evema.Grid.Rebuild();
 };
+
+Evema.Grid.Rebuild = function() {
+    let instance = Evema.Grid.Instance;
+    if ( instance === null ) {
+        console.warn( 'Can\'t rebuild grid, the grid instance is null!' );
+        return;
+    }
+
+    let options = Evema.Grid.Options;
+    instance.width = options.Width * options.CellWidth;
+    instance.height = options.Height * options.CellHeight;
+
+    Evema.Grid.Redraw();
+}
+
+Evema.Grid.Redraw = function() {
+    let grid = Evema.Grid;
+    grid.Clear();
+    grid.Draw();
+}
+
+Evema.Grid.Clear = function() {
+    let instance = Evema.Grid.Instance;
+    if ( instance === null ) {
+        console.warn( 'Can\'t clear grid, the grid instance is null!' );
+        return;
+    }
+
+    let context = Evema.Grid.Context;
+    let options = Evema.Grid.Options;
+    context.fillStyle = options.BackgroundColor;
+    context.fillRect( 0, 0, instance.width, instance.height );
+}
+
+Evema.Grid.Draw = function() {
+    let instance = Evema.Grid.Instance;
+    if ( instance === null ) {
+        console.warn( 'Can\'t draw grid, the grid instance is null!' );
+        return;
+    }
+
+    let context = Evema.Grid.Context;
+    let options = Evema.Grid.Options;
+    context.fillStyle = options.LineColor;
+    let cw = options.CellWidth;
+    let ch = options.CellHeight;
+
+    if ( options.Type === 'pixel' ) {
+        // Draw one pixel on each cell in left-top corner
+        for ( let y = 0, h = options.Height; y < h; y++ ) {
+            for ( let x = 0, w = options.Width; x < w; x++ ) {
+                context.fillRect( cw * x, ch * y, 1, 1 );
+            }
+        }
+    } else {
+        console.warn( `Unknown draw type "${options.Type}"` );
+    }
+}
