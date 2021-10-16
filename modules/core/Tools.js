02aa2e02de70afd0493b04eac59e51ad0a3b1c6c
DoggyXomaX <l_l_l_l-l_l_l_l@mail.ru>
2021-10-16 21:18:29 +0300

    Feature: Dynamic tools loading

--- modules/core/Tools.js
+++ modules/core/Tools.js
@@ -1,23 +1,43 @@
 Evema.Modules[ "Tools" ] = ( function() {
 
-const Tools = {};
+const Tools = {
+    List: [
+        { class: 'file-tool',      func: "Evema.Eval('Tools:File')"      },
+        { class: 'element-tool',   func: "Evema.Eval('Tools:Element')"   },
+        { class: 'power-tool',     func: "Evema.Eval('Tools:Power')"     },
+        { class: 'generator-tool', func: "Evema.Eval('Tools:Generator')" },
+        { class: 'about-tool',     func: "Evema.Eval('Tools:About')"     },
+        { class: 'options-tool',   func: "Evema.Eval('Tools:Options')"   },
+        { class: 'exit-tool',      func: "Evema.Eval('Tools:Exit')"      }
+    ]
+};
 
 Tools.Init = function() {
     const that = Tools;
 
-    Evema.SetLocal( that, 'Buttons', {
-        'tools__show'    : $( '.tools__show'    )[ 0 ],
-        'tools__hide'    : $( '.tools__hide'    )[ 0 ],
-        'file-tool'      : $( '.file-tool'      )[ 0 ],
-        'element-tool'   : $( '.element-tool'   )[ 0 ],
-        'power-tool'     : $( '.power-tool'     )[ 0 ],
-        'generator-tool' : $( '.generator-tool' )[ 0 ],
-        'about-tool'     : $( '.about-tool'     )[ 0 ],
-        'options-tool'   : $( '.options-tool'   )[ 0 ],
-        'exit-tool'      : $( '.exit-tool'      )[ 0 ]
-    } );
+    const panel = $( '.tools' )[ 0 ];
+    Evema.SetLocal( that, 'Panel', panel );
 
-    Evema.SetLocal( that, 'Panel', $( '.tools' )[ 0 ] );
+    const listLength = that.List.length;
+    const buttons = {
+        'tools__show': $( '.tools__show' )[ 0 ],
+        'tools__hide': $( '.tools__hide' )[ 0 ]
+    };
+
+    for ( let i = 0; i < listLength; i++ ) {
+        panel.appendChild( document.createElement( 'HR' ) );
+
+        const button = document.createElement( 'BUTTON' );
+        $( button )
+            .addClass( 'tools__button' )
+            .addClass( that.List[ i ].class )
+            .attr( 'onclick', that.List[ i ].func );
+
+        buttons[ that.List[ i ].class ] = button;
+        panel.appendChild( button );
+    }
+
+    Evema.SetLocal( that, 'Buttons', buttons );
 };
 
 Tools.HidePanel = function() {
