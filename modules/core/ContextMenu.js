79323e5ca863238279e6bfbe9fcae9e70f07a45b
DoggyXomaX <l_l_l_l-l_l_l_l@mail.ru>
2021-10-17 17:33:14 +0300

    PrepareFeature: Added show and hide context menu + prepare setters for options

--- modules/core/ContextMenu.js
+++ modules/core/ContextMenu.js
@@ -5,15 +5,36 @@ ContextMenu.Init = function() {
 
 }
 
+ContextMenu.Show = function() {
+    const that = ContextMenu;
+
+    Evema.SetLocal( that, "Visible", true );
+}
+
+ContextMenu.Hide = function() {
+    const that = ContextMenu;
+
+    Evema.SetLocal( that, "Visible", false );
+}
+
+ContextMenu.OnVisible = function() {
+    console.log( "ContextMenu.OnVisible" );
+}
+
 ContextMenu.Actions = {
-    'Init' : ContextMenu.Init
+    'Init' : ContextMenu.Init,
+    'Show' : ContextMenu.Show,
+    'Hide' : ContextMenu.Hide
 };
 
 ContextMenu.Options = {
     Standard: {
-
+        Visible: false
     },
-    Current: {}
+    Current: {},
+    Setters: {
+        Visible: ContextMenu.OnVisible
+    }
 }
 
 return ContextMenu; } )();
