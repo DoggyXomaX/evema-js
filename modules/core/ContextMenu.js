441fc2a19a908e6b3dc08e36300d7d3a15ede159
DoggyXomaX <l_l_l_l-l_l_l_l@mail.ru>
2021-10-17 17:25:45 +0300

    Create: ContextMenu module

---
+++ modules/core/ContextMenu.js
@@ -0,0 +1,19 @@
+Evema.Modules[ "ContextMenu" ] = ( function() {
+const ContextMenu = {};
+
+ContextMenu.Init = function() {
+
+}
+
+ContextMenu.Actions = {
+    'Init' : ContextMenu.Init
+};
+
+ContextMenu.Options = {
+    Standard: {
+
+    },
+    Current: {}
+}
+
+return ContextMenu; } )();
