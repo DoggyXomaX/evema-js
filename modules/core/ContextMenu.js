31bc6d513b2c8f2eb15e8606cf2fd9f6d648a690
DoggyXomaX <l_l_l_l-l_l_l_l@mail.ru>
2021-10-17 21:15:15 +0300

    Feature: context menu interraction

--- modules/core/ContextMenu.js
+++ modules/core/ContextMenu.js
@@ -1,6 +1,8 @@
 Evema.Modules[ "ContextMenu" ] = ( function() {
 const ContextMenu = {};
 
+ContextMenu.Instance = null;
+
 ContextMenu.Init = function() {
     const that = ContextMenu;
 
@@ -11,12 +13,29 @@ ContextMenu.Init = function() {
     }
 
     window.addEventListener( 'contextmenu', function( e ) { 
-        e.preventDefault(); 
+        e.preventDefault();
     }, false );
 
-    gridInstance.oncontextmenu = function() {
+    window.addEventListener( 'click', function( e ) {
+        Evema.SetLocal( that, 'Visible', false );
+    }, false );
+
+    gridInstance.oncontextmenu = function( e ) {
+        e.stopPropagation();
+        e.preventDefault();
+
+        // console.log( e );
+        Evema.SetLocal( that, 'Position', { x: e.x, y: e.y } );
         Evema.SetLocal( that, 'Visible', true );
     }
+
+    that.Instance = $( '.context-menu' )[ 0 ];
+    that.Instance.addEventListener( 'click', function( e ) {
+        e.stopPropagation();
+
+        // console.log( 'Context menu click' );
+    } );
+    Evema.SetLocal( that, 'Instance', that.Instance );
 }
 
 ContextMenu.Show = function() {
@@ -31,8 +50,15 @@ ContextMenu.Hide = function() {
     Evema.SetLocal( that, "Visible", false );
 }
 
-ContextMenu.OnVisible = function() {
-    console.log( "ContextMenu.OnVisible" );
+ContextMenu.OnVisible = function( source, value ) {
+    if ( value ) {
+        const position = Evema.GetLocal( source, 'Position' );
+        source.Instance.style.left = `${position.x}px`;
+        source.Instance.style.top = `${position.y}px`;
+        $( source.Instance ).removeClass( 'hidden' );
+    } else {
+        $( source.Instance ).addClass( 'hidden' );
+    }
 }
 
 ContextMenu.Actions = {
@@ -43,7 +69,9 @@ ContextMenu.Actions = {
 
 ContextMenu.Options = {
     Standard: {
-        Visible: false
+        Instance: null,
+        Visible: false,
+        Position: { x: 0, y: 0 }
     },
     Current: {},
     Setters: {
